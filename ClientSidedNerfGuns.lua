ea87f420811df673992031bfa2f14d222b11ea98
TheProxide <83253758+TheProxide@users.noreply.github.com>
2021-08-28 00:33:49 -0400

    Create ClientSidedNerfGuns.lua

---
+++ ClientSidedNerfGuns.lua
@@ -0,0 +1,14 @@
+require(game:GetService("ReplicatedStorage").Game.Notification).new({Text = "Because of Proxide's retarded ass, server sided nerf guns have been patched, enjoy the shitty client sided ones.", Duration = 5})
+
+local np = game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.NerfPistol
+local nr = game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.NerfRevolver
+local p = game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.Pistol
+local r = game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.Revolver
+
+p.Name = "DJ_PROXIDE_IN_THE_HOUUUSE"
+np.Name = "Pistol"
+game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.DJ_PROXIDE_IN_THE_HOUUUSE.Motion.Parent = game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.Pistol
+
+r.Name = "DJ_PROXIDE_IN_THE_HOUUUSE2"
+nr.Name = "Revolver"
+game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.DJ_PROXIDE_IN_THE_HOUUUSE2.Motion.Parent = game:GetService("ReplicatedStorage").Resource.ItemSystem.Models.Revolver
